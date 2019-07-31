a3ef41fae5b8a1eddb3e6f4c12685331b48fc76f
jw397 <jw397@126.com>
2019-07-31 14:26:50 +0800

    feat: 增加相对目录处理

--- gulpfile.js
+++ gulpfile.js
@@ -17,35 +17,56 @@ const chalk = require('chalk');
 /**
  * @param {String} fileName 要压缩的js文件名称
  * @param {String} filePath 要压缩的js文件路径
- * @param {String} destPath 压缩后的zip包路径
+ * @param {String} destDir 压缩后的zip包路径
  * @author jw
  * @date 2017-04-01
  */
-let zipFrame = function (fileName, filePath, destPath = 'dist') {
+let zipFrame = function (fileName, filePath, destDir = 'dist') {
   if (fileName.endsWith('.js')) {
     fileName = fileName.substring(0, fileName.length - 3);
   }
   let zipName = fileName + '_' + dateFormat(new Date(), 'yyyymmdd') + '.zip';
-  return gulp.src(path.resolve(filePath, fileName + '.js'))
+  return gulp.src(path.join(filePath, fileName + '.js'))
     .pipe(babel())
     .pipe(uglify())
     .pipe(rename('frame.js'))
     .pipe(zip(zipName))
-    .pipe(gulp.dest(destPath));
+    .pipe(gulp.dest(destDir));
 };
+
+let getAllFiles = async (filePath, arr = []) => {
+  let dirents = await readdir(filePath, {withFileTypes: true});
+  let promises = [];
+  dirents.forEach((dirent) => {
+    if (typeof dirent === 'string') {
+      throw new Error('nodejs version need update to v10.16');
+    }
+    if (dirent.isDirectory()) {
+      promises.push(getAllFiles(path.join(filePath, dirent.name), arr));
+    } else {
+      // console.log(filePath, dirent.name);
+      arr.push({
+        fileName: dirent.name,
+        filePath
+      });
+    }
+  });
+  return Promise.all(promises);
+};
+
 
 /**
  * 打包图层面板包
  */
-gulp.task('default', function () {
-  let filePath = './src/';
-  return readdir(filePath).then((arr) => {
-    return Promise.all(arr.map((fileName) => {
-      return zipFrame(fileName, filePath);
-    }));
-  }, () => {
-    console.log(`${chalk.red('没有找到文件')}`);
-  }).catch((err) => {
+gulp.task('default', async () => {
+  let arr = [];
+  let src = 'src';
+  let destDir = 'dist';
+  await getAllFiles(src, arr);
+  // console.log(arr);
+  return Promise.all(arr.map(({fileName, filePath}) => {
+    return zipFrame(fileName, filePath, filePath.replace(src, destDir));
+  })).catch((err) => {
     console.log(`${chalk.red(err)}`);
   });
 });
